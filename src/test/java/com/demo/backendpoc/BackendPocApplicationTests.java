48fdd91e064596dd1864d872258ee781754b7ef3
Ankur <ankurbakshi@Ankurs-MacBook-Pro.local>
2023-07-11 14:35:01 +0530

    First commit

---
+++ src/test/java/com/demo/backendpoc/BackendPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.backendpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
